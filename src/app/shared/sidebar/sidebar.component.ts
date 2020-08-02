6ff5803f480ce0d8a3159b920c0d9aceb2392cac
ConnerEnders <conner.enders@icloud.com>
2020-08-01 20:49:44 -0500

    Remove unused input

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router, ActivatedRoute } from "@angular/router";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
-  @Input() portal = false;
+export class SidebarComponent {
 
-  ngOnInit(): void {
-  }
 }
